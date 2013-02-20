4345d99889b4fa4301606fde2021a6a3a5315582
Danial <dani_khosravi@yahoo.com>
2013-02-20 23:22:46 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,169 @@
+/*
+Module Dependencies 
+*/
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    hash = require('./pass').hash;
+
+var app = express();
+
+/*
+Database and Models
+*/
+mongoose.connect("mongodb://localhost/myapp");
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    salt: String,
+    hash: String
+});
+
+var User = mongoose.model('users', UserSchema);
+/*
+Middlewares and configurations 
+*/
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.cookieParser('Authentication Tutorial '));
+    app.use(express.session());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+});
+
+app.use(function (req, res, next) {
+    var err = req.session.error,
+        msg = req.session.success;
+    delete req.session.error;
+    delete req.session.success;
+    res.locals.message = '';
+    if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+    if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+    next();
+});
+/*
+Helper Functions
+*/
+function authenticate(name, pass, fn) {
+    if (!module.parent) console.log('authenticating %s:%s', name, pass);
+
+    User.findOne({
+        username: name
+    },
+
+    function (err, user) {
+        if (user) {
+            if (err) return fn(new Error('cannot find user'));
+            hash(pass, user.salt, function (err, hash) {
+                if (err) return fn(err);
+                if (hash == user.hash) return fn(null, user);
+                fn(new Error('invalid password'));
+            });
+        } else {
+            return fn(new Error('cannot find user'));
+        }
+    });
+
+}
+
+function requiredAuthentication(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.redirect('/login');
+    }
+}
+
+function userExist(req, res, next) {
+    User.count({
+        username: req.body.username
+    }, function (err, count) {
+        if (count === 0) {
+            next();
+        } else {
+            req.session.error = "User Exist"
+            res.redirect("/signup");
+        }
+    });
+}
+
+/*
+Routes
+*/
+app.get("/", function (req, res) {
+
+    if (req.session.user) {
+        res.send("Welcome " + req.session.user.username + "<br>" + "<a href='/logout'>logout</a>");
+    } else {
+        res.send("<a href='/login'> Login</a>" + "<br>" + "<a href='/signup'> Sign Up</a>");
+    }
+});
+
+app.get("/signup", function (req, res) {
+    if (req.session.user) {
+        res.redirect("/");
+    } else {
+        res.render("signup");
+    }
+});
+
+app.post("/signup", userExist, function (req, res) {
+    var user = new User();
+    hash(req.body.password, function (err, salt, hash) {
+        if (err) throw err;
+        var user = new User({
+            username: req.body.username,
+            password : req.body.password,
+            salt: salt,
+            hash: hash,
+        }).save(function (err, newUser) {
+            if (err) throw err;
+            authenticate(newUser.username, newUser.password, function(err, user){
+                if(user){
+                    req.session.regenerate(function(){
+                        req.session.user = user;
+                        req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
+                        res.redirect('/');
+                    })
+                }
+            });
+        });
+    });
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.post("/login", function (req, res) {
+    authenticate(req.body.username, req.body.password, function (err, user) {
+        if (user) {
+
+            req.session.regenerate(function () {
+
+                req.session.user = user;
+                req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
+                res.redirect('/');
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your ' + ' username and password.';
+            res.redirect('/login');
+        }
+    });
+});
+
+app.get('/logout', function (req, res) {
+    req.session.destroy(function () {
+        res.redirect('/');
+    });
+});
+
+app.get('/profile', requiredAuthentication, function (req, res) {
+    res.send('Profile page of '+ req.session.user.username +'<br>'+' click to <a href="/logout">logout</a>');
+});
+
+
+http.createServer(app).listen(3000);
