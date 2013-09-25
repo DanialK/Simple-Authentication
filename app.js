9816b6132a6ecdeb07ca900a224dab3e79031b87
Danial <dani_khosravi@yahoo.com>
2013-09-25 19:33:54 +1000

    fixing problem of saving the plane password in db

--- app.js
+++ app.js
@@ -111,23 +111,24 @@ app.get("/signup", function (req, res) {
 });
 
 app.post("/signup", userExist, function (req, res) {
+    var password = req.body.password;
+    var username = req.body.username;
 
-    hash(req.body.password, function (err, salt, hash) {
+    hash(password, function (err, salt, hash) {
         if (err) throw err;
         var user = new User({
-            username: req.body.username,
-            password : req.body.password,
+            username: username,
             salt: salt,
             hash: hash,
         }).save(function (err, newUser) {
             if (err) throw err;
-            authenticate(newUser.username, newUser.password, function(err, user){
+            authenticate(newUser.username, password, function(err, user){
                 if(user){
                     req.session.regenerate(function(){
                         req.session.user = user;
                         req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
                         res.redirect('/');
-                    })
+                    });
                 }
             });
         });
